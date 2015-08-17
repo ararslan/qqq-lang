f875c29dc9f26571b7054f4b52e8e69e17f96e6b
Alex A <ararslan@comcast.net>
2015-08-16 22:11:16 -0700

    Initial commit

---
+++ cpp/qqq.cpp
@@ -0,0 +1,263 @@
+//
+//  qqq.cpp
+//  ??? (Programming Language)
+//
+//  Created by Alex A. and BrainSteel
+//  Based heavily on https://gist.github.com/migimunz/964338
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <vector>
+
+
+typedef char CELL;
+typedef std::vector<CELL> instruction_t;
+typedef instruction_t::iterator instruction_pointer;
+
+typedef std::vector<CELL> memory_t;
+typedef memory_t::iterator memory_pointer;
+
+
+struct environment {
+    memory_t memory;
+    instruction_t instructions;
+    
+    instruction_pointer ip;
+    memory_pointer mp;
+    
+    environment() : memory(3000) {
+        clear();
+        mp = memory.begin();
+    }
+    
+    void clear() {
+        instructions.clear();
+        ip = instructions.begin();
+    }
+};
+
+
+void interpret(environment &env);
+int from_line(environment &env, std::string &line, int open_quotes = 0);
+int from_stream(environment &env, std::istream &stream, int open_quotes = 0);
+void interactive_mode(environment &env);
+void print_cell(CELL cell);
+
+
+int main(int argc, const char* argv[]) {
+    
+    std::string filename;
+    
+    if (argc < 2) {
+        std::cout << "Enter a file:\n";
+        std::cin >> filename;
+    } else {
+        filename = argv[1];
+    }
+    
+    std::ifstream infile = std::ifstream(filename);
+    
+    if (infile.is_open()) {
+        
+        environment env;
+        
+        int unmatched_quotes = from_stream(env, infile);
+        
+        if (unmatched_quotes == 0)
+            interpret(env);
+        else
+            throw std::runtime_error("Um, match up your quotes and try again.");
+        
+        infile.close();
+        
+    } else {
+        throw std::runtime_error("Input file does not exist!");
+    }
+    
+    return 0;
+}
+
+
+/*
+ * Prints a memory cell, or hex representation if unprintable
+ */
+void print_cell(CELL cell) {
+    
+    if (isprint(cell))
+        std::cout << cell;
+    else
+        std::cout << "0x" << std::hex << (int)cell << std::dec;
+}
+
+
+/*
+ * Reads code from a line, returns number of unmatched quotes
+ */
+int from_line(environment &env, std::string &line, int open_quotes) {
+    
+    std::istringstream stream = std::istringstream(line);
+    
+    return from_stream(env, stream, open_quotes);
+}
+
+
+/*
+ * Reads code from a stream into env.instructions
+ */
+int from_stream(environment &env, std::istream &stream, int open_quotes) {
+    
+    if (open_quotes == 0)
+        env.instructions.push_back('\0');
+    
+    CELL cell;
+    
+    bool is_open = false;
+    
+    while (true) {
+        stream >> cell;
+        
+        if (!stream)
+            break;
+        
+        switch (cell) {
+            case '!':
+            case '?':
+            case '.':
+            case ',':
+            case '-':
+            case ';':
+                env.instructions.push_back(cell);
+                break;
+                
+            case '\'':
+                is_open = !is_open;
+                env.instructions.push_back(cell);
+                break;
+                
+            case '"':
+                if (is_open)
+                    open_quotes++;
+                else
+                    open_quotes--;
+                env.instructions.push_back(cell);
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    if (open_quotes == 0)
+        env.instructions.push_back('\0');
+    
+    return open_quotes;
+}
+
+
+/*
+ * Finds the closing quote, moves instruction pointer to it
+ */
+void find_closing(environment &env) {
+    int balance = 1;
+    bool is_open = true;
+    do {
+        env.ip++;
+        if (*env.ip == '\'')
+            is_open = !is_open;
+        if (*env.ip == '"') {
+            if (is_open)
+                balance++;
+            else
+                balance--;
+        }
+    } while (balance != 0);
+}
+
+
+/*
+ * Finds opening quote, moves instruction pointer to it
+ */
+void find_opening(environment &env) {
+    int balance = 0;
+    bool is_open = false;
+    do {
+        if (*env.ip == '\'')
+            is_open = !is_open;
+        if (*env.ip == '"') {
+            if (is_open)
+                balance++;
+            else
+                balance--;
+        }
+        env.ip--;
+    } while (balance != 0);
+}
+
+
+/*
+ * Interprets code
+ */
+void interpret(environment &env) {
+    
+    env.ip = env.instructions.begin();
+    
+    bool open_quote = false;
+    
+    while (env.ip != env.instructions.end()) {
+        
+        switch (*env.ip) {
+            case '.':
+                (*env.mp)++;
+                env.ip++;
+                break;
+                
+            case ',':
+                (*env.mp)--;
+                env.ip++;
+                break;
+                
+            case ';':
+                if (env.mp != (env.memory.end()--))
+                    env.mp++;
+                env.ip++;
+                break;
+                
+            case '-':
+                if (env.mp != env.memory.begin())
+                    env.mp--;
+                env.ip++;
+                break;
+                
+            case '!':
+                print_cell(*env.mp);
+                env.ip++;
+                break;
+                
+            case '?':
+                CELL cell;
+                std::cin >> cell;
+                (*env.mp) = cell;
+                env.ip++;
+                break;
+                
+            case '\'':
+                open_quote = !open_quote;
+                break;
+                
+            case '"':
+                if (open_quote && !(*env.mp))
+                    find_closing(env);
+                else
+                    find_opening(env);
+                env.ip++;
+                break;
+                
+            case '\0':
+                env.ip++;
+                break;
+        }
+    }
+}
